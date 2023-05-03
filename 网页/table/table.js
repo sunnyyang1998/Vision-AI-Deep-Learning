6bce7a4948f891ad6fb2351a0f4343b7d7364b5e
Qiaoxi <107198366+sunnyyang1998@users.noreply.github.com>
2023-05-04 02:20:22 +0930

    Delete 网页/table directory

--- 网页/table/table.js
+++
@@ -1,8 +0,0 @@
-const tableButtons = document.querySelectorAll('.table-button');
-
-tableButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const tableNumber = event.target.dataset.table;
-    window.location.href = `../order/order.html?table=${tableNumber}`;
-  });
-});
