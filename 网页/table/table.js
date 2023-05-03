b5303cb51223895f0acf3380f274d3143a7ab167
Qiaoxi <107198366+sunnyyang1998@users.noreply.github.com>
2023-05-04 00:51:15 +0800

    Add files via upload

---
+++ 网页/table/table.js
@@ -0,0 +1,16 @@
+firebase.initializeApp(firebaseConfig);
+
+function bindTableButtons() {
+  const tableButtons = document.querySelectorAll('.table-button');
+
+  tableButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const tableNumber = event.target.dataset.table;
+      window.location.href = `../menu/menu.html?table=${tableNumber}`;
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  bindTableButtons();
+});
