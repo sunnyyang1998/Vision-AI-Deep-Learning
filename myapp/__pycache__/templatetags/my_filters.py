3af3e4f2093227795675cdb84d5f106fc58065c9
Qiaoxi <107198366+sunnyyang1998@users.noreply.github.com>
2023-06-04 18:20:24 +0930

    Delete myapp/__pycache__ directory

--- myapp/__pycache__/templatetags/my_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ..models import Product
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    product = Product.objects.get(id=key)
-    return product
